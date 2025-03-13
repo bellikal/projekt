7e36fd2b779295eaab9a94ba3ff6f811c3550af7
Belli <belliz@me.com>
2025-03-13 13:40:13 +0100

    Button component

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,24 @@
 {{--    <x-headline size="6">Hello world!</x-headline>--}}
 {{--</div>--}}
 
-    <x-header.header></x-header.header>
+{{--    <x-header.header></x-header.header>--}}
 {{--    <x-sidenav.sidenav></x-sidenav.sidenav>--}}
+
+    <div class="bg-white w-full h-full p-48">
+        <div class=" w-fit grid grid-cols-2 gap-2">
+            <x-button >Button</x-button>
+            <x-button disabled >Button</x-button>
+
+            <x-button secondary >Button</x-button>
+            <x-button disabled secondary >Button</x-button>
+
+            <x-button outline >Button</x-button>
+            <x-button disabled outline >Button</x-button>
+
+            <x-button danger >Button</x-button>
+            <x-button disabled danger >Button</x-button>
+        </div>
+
+    </div>
+
 </x-app-layout>
