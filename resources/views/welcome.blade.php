c8cd139111110ae8e05433667693237ab19a54ba
Belli <belliz@me.com>
2025-03-14 11:10:28 +0100

    TestMail

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,11 +11,17 @@
 {{--    <x-header.header></x-header.header>--}}
 {{--    <x-sidenav.sidenav></x-sidenav.sidenav>--}}
 
+    <x-headline size="1" class="bg-blue-200">Hallo</x-headline>
+    <x-headline size="2" class="bg-blue-200">Hallo</x-headline>
+    <x-headline size="3" class="bg-blue-200">Hallo</x-headline>
+    <x-headline size="4" class="bg-blue-200">Hallo</x-headline>
+    <x-headline size="5" class="bg-blue-200">Hallo</x-headline>
+    <x-headline size="6" class="bg-blue-200">Hallo</x-headline>
 
     <div class="p-24 flex gap-2">
         <x-badge small class="w-fit h-fit">Small</x-badge>
         <x-badge class="w-fit h-fit">Default</x-badge>
-        <x-badge large class="w-fit h-fit">Large</x-badge>
+        <x-badge large class="w-fit h-fit bg-green-500">Large</x-badge>
     </div>
 
 </x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,16 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Mail\TestEmail;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/send-test-email', function () {
+    Mail::to('belliz@me.com')->send(new TestEmail());
+    return 'Email wurde gesendet';
 });
 
 Route::get('/dashboard', function () {
