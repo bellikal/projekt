e89bf968cd78e2c4129310bb267b81722ee246c9
Belli <belliz@me.com>
2025-03-14 11:10:03 +0100

    Create test.blade.php

---
+++ resources/views/emails/test.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Test E-Mail</title>
+</head>
+<body>
+    <h1>Hallo, das ist eine Test-E-mail</h1>
+    <p>Wenn du diese E-Mail siehst, funktioniert alles!</p>
+</body>
+</html>
